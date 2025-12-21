55fd62c8e8fa4b3dc33c981ae44e3d609d43487e
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-12-21 15:00:26 +0530

    no string lib, i love builtins

--- ch1/ch1-penguins/src/main.rs
+++ ch1/ch1-penguins/src/main.rs
@@ -9,24 +9,24 @@ fn main() {                 // <1> <2>
 
   let records = penguin_data.lines();
 
-  for (i, record) in records.enumerate() {
-    if i == 0 || record.trim().len() == 0 {  // <3>
+  for(i, record) in records.enumerate(){
+    if i==0 || record.trim().len()==0{
       continue;
     }
 
-    let fields: Vec<_> = record     // <4>
-      .split(',')                   // <5>
-      .map(|field| field.trim())    // <6>
-      .collect();                   // <7>
+    let fields: Vec<_> = record
+      .split(',') // split the line based on commas.
+      .map(|field| field.trim()) // what is map?
+      .collect(); // builds a collection of fields
 
-    if cfg!(debug_assertions) {              // <8>
-      eprintln!("debug: {:?} -> {:?}",
-	             record, fields);            // <9>
+    // cfg is a macro that checks configurations at compile time.
+    if cfg!(debug_assertions) {
+      eprintln!("debug: {:?} -> {:?}", record, fields); // print to std err
     }
 
     let name = fields[0];
-    if let Ok(length) = fields[1].parse::<f32>() { // <10>
-        println!("{}, {}cm", name, length);        // <11>
+    if let Ok(length) = fields[1].parse::<f32>(){
+      println!("{}, {}cm",name, length); // print to std out
     }
   }
 }
