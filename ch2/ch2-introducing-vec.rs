927281a4963c572d6db16a4130d71050e739ab1d
Shiwam Jaiswal <shiwam.jaiswal@velotio.com>
2025-12-25 09:48:57 +0530

    better algo

--- ch2/ch2-introducing-vec.rs
+++ ch2/ch2-introducing-vec.rs
@@ -14,8 +14,8 @@ through millions of pages?";
   // let mut ctx: Vec<Vec<(
               //  usize, String)>> = vec![];          // <2>
   let mut ctx = vec![];
-
-  for (i, line) in haystack.lines().enumerate() {  // <3>
+  let lines: Vec<&str> = haystack.lines().collect();
+  for (i, line) in lines.iter().enumerate() {  // <3>
     if line.contains(needle) {
       tags.push(i);
 
@@ -28,20 +28,24 @@ through millions of pages?";
     return;
   }
 
-  for (i, line) in haystack.lines().enumerate() {  // <6>
+  // for (i, line) in haystack.lines().enumerate() {  // <6>
     for (j, tag) in tags.iter().enumerate() {
       let lower_bound =
 	      tag.saturating_sub(ctx_lines);           // <7>
       let upper_bound =
 	      tag + ctx_lines;
-
-      if (i >= lower_bound) && (i <= upper_bound) {
-          let line_as_string = String::from(line); // <8>
-          let local_ctx = (i, line_as_string);
-          ctx[j].push(local_ctx);
+      for i in lower_bound..=upper_bound{
+        let line_as_string = String::from(lines[i]);
+        let local_ctx = (i, line_as_string);
+        ctx[j].push(local_ctx);
       }
+      // if (i >= lower_bound) && (i <= upper_bound) {
+      //     let line_as_string = String::from(line); // <8>
+      //     let local_ctx = (i, line_as_string);
+      //     ctx[j].push(local_ctx);
+      // }
     }
-  }
+  // }
 
   for local_ctx in ctx.iter() {
     for &(i, ref line) in local_ctx.iter() {       // <9>
